f69e83fd194d53f2a1b2d53a0696a9915c5dd1b6
Christopher Adolphe <christopher.adolphe@gmail.com>
2025-02-19 17:23:04 +0400

    feat: scaffold minimal nuxt application

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
+const { data } = await useFetch(`${config.public.baseJobApi}/filters`);
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt VPS Test</h1>
+
+    <br />
+
+    <pre>Filters: {{ data }}</pre>
+  </div>
+</template>
+
